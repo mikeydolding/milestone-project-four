a9199386150ae58c82b62af3df4eb4fc80b0859b
tekkoop <tekkoop@tutanota.com>
2023-06-10 18:57:11 +0300

    Customised admin

--- items/admin.py
+++ items/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Item, Category
 # Register your models here.
 
-admin.site.register(Item)
-admin.site.register(Category)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
