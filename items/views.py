1d426fb3afc6daccad359c9f67b243481cb4c4cb
tekkoop <tekkoop@tutanota.com>
2023-06-11 11:58:12 +0300

    Added product details.html

--- items/views.py
+++ items/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 # Create your views here.
 
 def all_items(request):
-    """ A view to show all items, including sort and search queries """
+    """ A view to show items, including sort and search queries """
 
     items = Item.objects.all()
 
@@ -12,3 +12,15 @@ def all_items(request):
     }
      
     return render(request, 'items/items.html', context)
+
+def item_detail(request, item_id):
+
+    """ A view to show individual items """
+
+    item = get_object_or_404(Item, pk=item_id)
+ 
+    context = {
+        'item': item,
+    }
+     
+    return render(request, 'items/item_detail.html', context)
