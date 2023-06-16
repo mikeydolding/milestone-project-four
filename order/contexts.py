9a29b590e2951df8704f2af067690eb5ae1a893b
tekkoop <tekkoop@tutanota.com>
2023-06-16 10:32:18 +0300

    Re-edit from 022

--- order/contexts.py
+++ order/contexts.py
@@ -3,35 +3,43 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from items.models import Item
 
-
 def order_contents(request):
 
-    orderItems = []
+    order_items = []
     total = 0
-    product_count = 0
+    item_count = 0
     order = request.session.get('order', {})
 
-    for orderItem_id, orderItem_data in order.items():
-        if isinstance(orderItem_data, int):
-            item = get_object_or_404(Item, pk=orderItem_id)
-            total += orderItem_data * item.price
-            product_count += orderItem_data
-            orderItems.append({
-                'orderItem_id': orderItem_id,
-                'quantity': orderItem_data,
-                'item': item,
-            })
-        else:
-            item = get_object_or_404(Item, pk=orderItem_id)
-            for size, quantity in orderItem_data['orderItems_by_size'].orderItems():
-                total += quantity * item.price
-                product_count += quantity
-                orderItems.append({
-                    'orderItem_id': orderItem_id, 
-                    'quantity': orderItem_data,
-                    'item': item,
-                    'size': size,
-                })
+    for orderItem_id, quantity in order.items():
+        item = get_object_or_404(Item, pk=orderItem_id)
+        total += quantity * item.price
+        item_count += quantity
+        order_items.append({
+            'orderItem_id': orderItem_id,
+            'quantity': quantity,
+            'item': item,
+        })
+    #for orderItem_id, orderItem_data in order.items():
+    #    if isinstance(orderItem_data, int):
+    #        item = get_object_or_404(Item, pk=orderItem_id)
+    #        total += orderItem_data * item.price
+    #        item_count += orderItem_data
+    #        order_items.append({
+    #            'orderItem_id': orderItem_id,
+    #            'quantity': orderItem_data,
+    #            'item': item,
+    #        })
+    #    else:
+    #        item = get_object_or_404(Item, pk=orderItem_id)
+    #        for size, quantity in orderItem_data['items_by_size'].items():
+    #            total += quantity * item.price
+    #            item_count += quantity
+    #            order_items.append({
+    #                'orderItem_id': orderItem_id,
+    #                'quantity': orderItem_data,
+    #                'item': item,
+    #                'size': size,
+    #            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -39,13 +47,13 @@ def order_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
-        'orderItems': orderItems,
+        'order_items': order_items,
         'total': total,
-        'product_count': product_count,
+        'item_count': item_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
--- order/views.py
+++ order/views.py
@@ -13,25 +13,30 @@ def add_to_order(request, orderItem_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
-    if 'orderItem_size' in request.POST:
-        size = request.POST['orderItem_size']
+    if 'item_size' in request.POST:
+        size = request.POST['item_size']
     order = request.session.get('order', {})
 
-    if size:
-        if orderItem_id in list(order.keys()):
-            if size in order[orderItem_id]['orderItems_by_size'].keys():
-                order[orderItem_id]['orderItems_by_size'][size] += quantity
-            else:      
-                order[orderItem_id]['orderItems_by_size'][size] = quantity
-        else:
-            order[orderItem_id] = {'orderItems_by_size': {size: quantity}}
-    else:        
-        if orderItem_id in list(order.keys()):
-             order[orderItem_id] += quantity
-        
-        else:
-            order[orderItem_id] = quantity
+
+    if orderItem_id in list(order.keys()):
+        order[orderItem_id] += quantity
+    else:
+        order[orderItem_id]= quantity
+    #if size:
+    #    if orderItem_id in list(order.keys()):
+    #        if size in order[orderItem_id]['orderItems_by_size'].keys():
+    #            order[orderItem_id]['orderItems_by_size'][size] += quantity
+    #        else:
+    #            order[orderItem_id]['orderItems_by_size'][size] = quantity
+    #    else:
+    #        order[orderItem_id] = {'orderItems_by_size': {size: quantity}}
+    #else:
+    #    if orderItem_id in list(order.keys()):
+    #        order[orderItem_id] += quantity
+    #    else:
+    #        order[orderItem_id] = quantity
 
     request.session['order'] = order
+    print(request.session['order'])
     return redirect(redirect_url)
     
