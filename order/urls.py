180605f37163354d7f45e7cccb1a505dafaf9286
tekkoop <tekkoop@tutanota.com>
2023-06-18 12:49:01 +0300

    Fixed remove Item link

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_order, name='view_order'),
     path('add/<orderItem_id>/', views.add_to_order, name='add_to_order'),
-    #path('adjust/<orderItem_id>/', views.adjust_order, name='adjust_order'),
-    #path('remove/<orderItem_id>/', views.remove_from_order, name='remove_from_order'),
+    path('update/<orderItem_id>/', views.update_order, name='update_order'),
+    path('remove/<orderItem_id>/', views.remove_from_order, name='remove_from_order'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -18,11 +18,6 @@ def add_to_order(request, orderItem_id):
         print('size',request.POST['item_size'])
     order = request.session.get('order', {})
 
-    #if orderItem_id in list(order.keys()):
-    #    order[orderItem_id] += quantity
-    #else:
-    #    order[orderItem_id]= quantity
-
     if size:
         if orderItem_id in list(order.keys()):
             if size in order[orderItem_id]['orderItems_by_size'].keys():
@@ -37,22 +32,64 @@ def add_to_order(request, orderItem_id):
         else:
             order[orderItem_id] = quantity
 
-
-    #if size:
-    #    if orderItem_id in list(order.keys()):
-    #        if size in order[orderItem_id]['orderItems_by_size'].keys():
-    #            order[orderItem_id]['orderItems_by_size'][size] += quantity
-    #        else:
-    #            order[orderItem_id]['orderItems_by_size'][size] = quantity
-    #    else:
-    #        order[orderItem_id] = {'orderItems_by_size': {size: quantity}}
-    #else:
-    #    if orderItem_id in list(order.keys()):
-    #        order[orderItem_id] += quantity
-    #    else:
-    #        order[orderItem_id] = quantity
-
     request.session['order'] = order
     print(request.session['order'])
     return redirect(redirect_url)
     
+def update_order(request, orderItem_id):
+    """ Update quantity of the specified product to the shopping order """
+    quantity = int(request.POST.get('quantity'))
+
+
+    size = None   
+    if 'item_size' in request.POST:  
+        size = request.POST['item_size']
+        print('size',request.POST['item_size'])
+    order = request.session.get('order', {})
+
+    if size:
+        if quantity > 0:
+            order[orderItem_id]['orderItems_by_size'][size] = quantity
+        else:
+            del order[orderItem_id]['orderItems_by_size'][size]
+            if not order[orderItem_id]['orderItems_by_size']:
+                order.pop(orderItem_id)
+    else:
+        if quantity > 0:
+            order[orderItem_id] = quantity  
+        else:
+            order.pop(orderItem_id)
+
+
+    request.session['order'] = order
+    return redirect(reverse('view_order'))
+
+def remove_from_order(request, orderItem_id):
+    """Remove the item from the shopping order"""
+    print('request data',request.POST)
+    print('orderItem_id',orderItem_id)
+
+    try:
+        size = None
+        if 'item_size' in request.POST:
+            size = request.POST['item_size']
+        order = request.session.get('order', {})
+        print('item_size in request.POST',order)
+
+        if size:
+            print('size',order[orderItem_id]['orderItems_by_size'][size])
+
+            del order[orderItem_id]['orderItems_by_size'][size]
+            if not order[orderItem_id]['orderItems_by_size']:
+                order.pop(orderItem_id)
+
+
+
+        else:
+            order.pop(orderItem_id)
+
+        request.session['order'] = order
+        return HttpResponse(status=200)
+ 
+    except Exception as e:
+        return HttpResponse(status=500)
